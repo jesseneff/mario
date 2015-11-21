2b1341b982ecdd2c5a0cd89dc7ca0bd372b8cc27
Jesse Neff <jesse.neff91@gmail.com>
2015-11-21 13:06:27 -0500

    PSET 2 update

--- src/com/mario/Main.java
+++ src/com/mario/Main.java
@@ -1,5 +1,8 @@
 package com.mario;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 
@@ -9,7 +12,21 @@ public class Main {
 
         Scanner input = new Scanner(System.in);
         int n;
+        String choice;
 
+        //start input 1
+        do {
+            System.out.print("Will this be written to a file? (Y|N): ");
+            choice = input.next().toLowerCase();
+            System.out.println();
+        }
+        while (!choice.equals("yes") &&
+                !choice.equals("no") &&
+                !choice.equals("y") &&
+                !choice.equals("n")
+                );
+
+        //start input 2
         do {
             System.out.print("Please enter the height of the pyramid (0 <= n <= 23): ");
             n = input.nextInt();
@@ -17,12 +34,30 @@ public class Main {
         }
         while (n < 0 || n > 23);
 
-        for (int i = 1; i < n ; i++){
-            String spaces = new String(new char[(n-1)-i]).replace('\0', ' ');
-            String hashes = new String(new char[i+1]).replace('\0', '#');
+        //create file
+        if (choice.equals("y") || choice.equals("yes")){
 
-            System.out.println(spaces+hashes);
+            String fileName = "output.txt";
+
+            try (FileWriter fileWriter = new FileWriter(new File(fileName));) {
+                for (int i = 1; i < n ; i++) {
+                    String spaces = new String(new char[(n - 1) - i]).replace('\0', ' ');
+                    String hashes = new String(new char[i + 1]).replace('\0', '#');
+                    fileWriter.write(spaces + hashes);
+                    fileWriter.write(System.lineSeparator());
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            System.out.println("File saved.");
         }
-
+        //output to console
+        else{
+            for (int i = 1; i < n ; i++){
+                String spaces = new String(new char[(n-1)-i]).replace('\0', ' ');
+                String hashes = new String(new char[i+1]).replace('\0', '#');
+                System.out.println(spaces+hashes);
+            }
+        }
     }
 }
