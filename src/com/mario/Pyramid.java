df8eda145544c998935c3ce53213afd302ed5a59
Jesse Neff <jesse.neff91@gmail.com>
2015-11-21 21:57:12 -0500

    changed where some things happen slightly

--- src/com/mario/Pyramid.java
+++ src/com/mario/Pyramid.java
@@ -3,12 +3,12 @@ package com.mario;
 import java.util.ArrayList;
 
 public class Pyramid extends ArrayList<String>{
-        public static Pyramid makePyramid(int n){
+        public Pyramid makePyramid(int n){
             Pyramid pyramid = new Pyramid();
             for (int i = 1; i < n ; i++){
                 String spaces = new String(new char[(n-1)-i]).replace('\0', ' ');
                 String hashes = new String(new char[i+1]).replace('\0', '#');
-                pyramid.add(spaces+hashes);
+                pyramid.add(spaces+hashes+"\n");
             }
 
             return pyramid;
@@ -16,11 +16,9 @@ public class Pyramid extends ArrayList<String>{
 
         @Override
         public String toString(){
-            int n = this.size();
             StringBuilder s = new StringBuilder();
 
-            for (int i = 0; i < n; i++) s.append(this.get(i) + "\n");
-
+            for (String s1 : this) s.append(s1);
 
             return s.toString();
         }
